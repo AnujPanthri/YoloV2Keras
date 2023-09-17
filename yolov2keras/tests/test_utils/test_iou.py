2611c0f7cde62c3b7ec4f0c8e37a617a41340124
Ankur Panthri <panthrianuj@gmail.com>
2023-09-17 19:54:47 +0530

    added testing code for iou.GetIoU function

--- yolov2keras/tests/test_utils/test_iou.py
+++ yolov2keras/tests/test_utils/test_iou.py
@@ -14,15 +14,38 @@
 
 import pytest
 import numpy as np
-
+import tensorflow.keras.backend as K
+import tensorflow as tf
 from yolov2keras.utils import iou
 
 
 # TODO(AnujPanthri): Test `iou.GetIoU` for `np.ndarray` here.
 def test_iou_numpy():
-  pass
-
+    true_boxes = np.array([[10, 10, 20, 20],[15, 10, 20, 20], [30, 30, 40, 40],[40, 40, 20, 20]])
+    pred_boxes = np.array([[10, 10, 20, 20]])
+    iou_tf = iou.GetIoU(true_boxes, pred_boxes)
+    
+    assert tf.reduce_all(iou_tf==iou.GetIoU(true_boxes,np.tile(pred_boxes,[true_boxes.shape[0],1]))).numpy()  # checking if broadcasting is working
+    assert tf.reduce_all(iou_tf==iou.GetIoU(pred_boxes,true_boxes)).numpy()  # checking if order doesn't matters
+    assert iou_tf.shape==[true_boxes.shape[0]]
+    assert tf.reduce_all(tf.logical_and(iou_tf>=0,iou_tf<=1)).numpy()  # should be between 0 and 1
+    assert abs(iou_tf[0]-1)<1e-4  # complete intersection
+    assert abs(iou_tf[1]-(300/500))<1e-4  # test 1 intersection
+    assert abs(iou_tf[2]-(100/1900))<1e-4  # test 2 intersection
+    assert iou_tf[3]<1e-4  # no intersection
 
 # TODO(AnujPanthri): Test `iou.GetIoU` for `tf.Tensor` here.
 def test_iou_tf():
-  pass
+    
+    true_boxes = K.variable([[10, 10, 20, 20],[15, 10, 20, 20], [30, 30, 40, 40],[40, 40, 20, 20]])
+    pred_boxes = K.variable([[10, 10, 20, 20]])
+    iou_tf = iou.GetIoU(true_boxes, pred_boxes)
+    
+    assert tf.reduce_all(iou_tf==iou.GetIoU(true_boxes,tf.tile(pred_boxes,[true_boxes.shape[0],1]))).numpy()  # checking if broadcasting is working
+    assert tf.reduce_all(iou_tf==iou.GetIoU(pred_boxes,true_boxes)).numpy()  # checking if order doesn't matters
+    assert iou_tf.shape==[true_boxes.shape[0]]
+    assert tf.reduce_all(tf.logical_and(iou_tf>=0,iou_tf<=1)).numpy()  # should be between 0 and 1
+    assert abs(iou_tf[0]-1)<1e-4  # complete intersection
+    assert abs(iou_tf[1]-(300/500))<1e-4  # test 1 intersection
+    assert abs(iou_tf[2]-(100/1900))<1e-4  # test 2 intersection
+    assert iou_tf[3]<1e-4  # no intersection
--- yolov2keras/utils/iou.py
+++ yolov2keras/utils/iou.py
@@ -18,59 +18,60 @@ import numpy as np
 import tensorflow as tf
 import tensorflow.keras.backend as K
 
+
 # Do we really need to support `np.ndarray` for calculating `IoU` when
 # `yolov2keras` uses `tf.Tensor`?
-def GetIoU(
-  y_true: tf.Tensor , y_pred: tf.Tensor
-) -> tf.Tensor:
-  """Calculate Intersection over Union (IoU) between sets of bounding boxes.
+def GetIoU(y_true: tf.Tensor, y_pred: tf.Tensor) -> tf.Tensor:
+    """Calculate Intersection over Union (IoU) between sets of bounding boxes.
 
-  See the Notes: sections to look at the usage with `tf.Tensor`.
+      See the Notes: sections to look at the usage with `tf.Tensor`.
 
-  Args:
-    y_true: Ground truth bounding boxes in the format [center_x, center_y, w, h].
-    y_pred: Predicted bounding boxes in the format [center_x, center_y, w, h].
+      Args:
+        y_true: Ground truth bounding boxes in the format [center_x, center_y, w, h].
+        y_pred: Predicted bounding boxes in the format [center_x, center_y, w, h].
 
-  Returns:
-    numpy.ndarray: An array containing IoU values for each pair of boxes.
+      Returns:
+        numpy.ndarray: An array containing IoU values for each pair of boxes.
 
-  Notes:
-    - IoU values range from 0 (no overlap) to 1 (perfect overlap).
-    - Use `tf.compat.v1.enable_eager_execution()` to enable eager execution.
-      Once eager execution is enabled, operations are executed as they are
-      defined and Tensor objects hold concrete values, which can be accessed as
-      `numpy.ndarray``s through the `numpy()` method.
+      Notes:
+        - IoU values range from 0 (no overlap) to 1 (perfect overlap).
+        - Use `tf.compat.v1.enable_eager_execution()` to enable eager execution.
+          Once eager execution is enabled, operations are executed as they are
+          defined and Tensor objects hold concrete values, which can be accessed as
+          `numpy.ndarray``s through the `numpy()` method.
 
-  Examples:
+      Examples:
 
-    Calculate IoU using TensorFlow tensors:
+        Calculate IoU using TensorFlow tensors:
 
-    >>> import tensorflow.keras.backend as K
-    >>> true_boxes = K.variable([[12, 10, 20, 20], [30, 30, 40, 40]])
-    >>> pred_boxes = K.variable([[12, 12, 20, 20]])
-    >>> iou_tf = GetIoU(true_boxes, pred_boxes)
-    >>> print(iou_tf)
-    [0.8181818 0.0775862]
+        >>> import tensorflow.keras.backend as K
+        >>> true_boxes = K.variable([[12, 10, 20, 20], [30, 30, 40, 40]])
+        >>> pred_boxes = K.variable([[12, 12, 20, 20]])
+        >>> iou_tf = GetIoU(true_boxes, pred_boxes)
+        >>> print(iou_tf)
+        [0.8181818 0.0775862]
 
-    Calculate IoU using TensorFlow tensors converted to NumPy arrays:
-  """
-  box1_x1 = y_true[:, 0] - y_true[:, 2] / 2
-  box1_y1 = y_true[:, 1] - y_true[:, 3] / 2
-  box1_x2 = y_true[:, 0] + y_true[:, 2] / 2
-  box1_y2 = y_true[:, 1] + y_true[:, 3] / 2
+        Calculate IoU using TensorFlow tensors converted to NumPy arrays:
+    """
+    box1_x1 = y_true[:, 0] - y_true[:, 2] / 2
+    box1_y1 = y_true[:, 1] - y_true[:, 3] / 2
+    box1_x2 = y_true[:, 0] + y_true[:, 2] / 2
+    box1_y2 = y_true[:, 1] + y_true[:, 3] / 2
 
-  box2_x1 = y_pred[:, 0] - y_pred[:, 2] / 2
-  box2_y1 = y_pred[:, 1] - y_pred[:, 3] / 2
-  box2_x2 = y_pred[:, 0] + y_pred[:, 2] / 2
-  box2_y2 = y_pred[:, 1] + y_pred[:, 3] / 2
+    box2_x1 = y_pred[:, 0] - y_pred[:, 2] / 2
+    box2_y1 = y_pred[:, 1] - y_pred[:, 3] / 2
+    box2_x2 = y_pred[:, 0] + y_pred[:, 2] / 2
+    box2_y2 = y_pred[:, 1] + y_pred[:, 3] / 2
 
-  xmins = K.maximum(box1_x1, box2_x1)
-  ymins = K.maximum(box1_y1, box2_y1)
-  xmaxs = K.minimum(box1_x2, box2_x2)
-  ymaxs = K.minimum(box1_y2, box2_y2)
+    xmins = K.maximum(box1_x1, box2_x1)
+    ymins = K.maximum(box1_y1, box2_y1)
+    xmaxs = K.minimum(box1_x2, box2_x2)
+    ymaxs = K.minimum(box1_y2, box2_y2)
 
-  intersection = K.clip((xmaxs - xmins), 0, None) * K.clip((ymaxs - ymins), 0, None)
-  union = (box1_x2 - box1_x1) * (box1_y2 - box1_y1) + (box2_x2 - box2_x1) * (box2_y2 - box2_y1)
-  ious = intersection / (union - intersection + 1e-6)
+    intersection = K.clip((xmaxs - xmins), 0, None) * K.clip(
+        (ymaxs - ymins), 0, None)
+    union = (box1_x2 - box1_x1) * (box1_y2 - box1_y1) + (box2_x2 - box2_x1) * (
+        box2_y2 - box2_y1)
+    ious = intersection / (union - intersection + 1e-6)
 
-  return ious
+    return ious
